e711e49e614257d758ffa35716e6bb08c9a1dc66
Douglas Webster <douglas.webster2107@gmail.com>
2022-12-15 18:17:52 +0000

    Day 14 scaffolded

---
+++ 2022/day14/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+#include <chrono>
+
+#define TESTING false
+
+int main(int, char **)
+{
+    auto time_start = std::chrono::high_resolution_clock::now();
+    std::string file_name = (TESTING) ? "../test.txt" : "../input.txt";
+    std::ifstream data{file_name};
+
+    if (!data)
+    {
+        std::cerr << "input.txt could not be opened for reading\n ";
+        return 1;
+    }
+
+    std::vector<std::string> inputs;
+    while (data)
+    {
+    
+    }
+
+    
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - time_start);
+
+    std::cout << "Time taken by program: " << duration.count() << " microseconds"
+              << "\n";
+    return 0;
+}
