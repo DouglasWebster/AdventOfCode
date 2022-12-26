ec7215e0643d4bf03d4f6aa81ef96ae7ee19b685
Douglas Webster <douglas.webster2107@gmail.com>
2022-12-26 11:58:52 +0000

    Scaffolded day 16

---
+++ 2022/day16/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include <chrono>
+
+#define TESTING false
+
+int main(int, char **)
+{
+    auto time_start = std::chrono::high_resolution_clock::now();
+    std::string file_name = (TESTING) ? "../test.txt" : "../input.txt";
+    std::ifstream data{file_name};
+
+    while (data)
+    {
+      
+    }
+
+    
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - time_start);
+
+    std::cout << "Time taken by program: " << duration.count() << " microseconds"
+              << "\n";
+    return 0;
+}
