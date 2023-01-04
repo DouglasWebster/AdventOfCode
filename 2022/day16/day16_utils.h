17ce9a42c01893ab315634cfc23717bbfbaec8cb
Douglas Webster <douglas.webster2107@gmail.com>
2023-01-04 10:20:05 +0000

    Ported to seperate helper files

--- 2022/day16/day16_utils.h
+++ 2022/day16/day16_utils.h
@@ -1,17 +1,58 @@
-void build_edges(std::set<Edge>& edges, const Valves& valves) {
-  std::map<std::string, int> edge_map;
+#ifndef DSW_DAY16_UTILS_H
+#define DSW_DAY16_UTILS_H
 
-  for (int index{0}; auto valve : valves)
-    edge_map.insert(std::make_pair(valve.name, index++));
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <regex>
+#include <string>
+#include <set>
 
-  for (auto valve : valves) {
-    int source{(edge_map[valve.name])};
-    for (auto neighbour : valve.neighbours) {
-      int destination{edge_map[neighbour]};
-      if (source < destination)  // prevent duplicate paths
-        edges.insert(Edge(source, destination));
-      else
-        edges.insert(Edge(destination, source));
-    }
-  }
-}
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/johnson_all_pairs_shortest.hpp>
+#include <boost/graph/named_function_params.hpp>
+#include <boost/graph/properties.hpp>
+#include <boost/property_map/property_map.hpp>
+
+#define TESTING true
+
+#if TESTING
+#define V 10
+#else
+#define V 58
+#endif  // TESTING
+
+struct ValveInfo {
+  std::string name{};
+  int flow{};
+  std::vector<std::string> neighbours;
+};
+
+using Valves = std::vector<ValveInfo>;
+using Edge = std::pair<int, int>;
+
+struct VertexData {
+  std::string node_name;
+  int flow{0};
+};
+
+struct EdgeData {
+  std::string edge_name;
+  double dist;
+};
+
+using CavernGraph = boost::adjacency_list<
+    boost::vecS, boost::vecS, boost::undirectedS, VertexData,
+    boost::property<boost::edge_weight_t, int,
+                    boost::property<boost::edge_weight2_t, int>>>;
+
+void build_edges(std::set<Edge>& edges, const Valves& valves);
+void parse_input(Valves& valves);
+void build_vertices(std::vector<VertexData>& vertices, Valves& valves);
+void draw_graph(const CavernGraph& graph, const Valves& valves);
+void print_distance_matrix(const int D[][V]);
+void print_flowing_distance_matrix(const int D[][V],
+                                   const std::vector<int>& working_valves);
+
+#endif //DSW_DAY16_UTILS_H
--- 2022/day16/main.cpp
+++ 2022/day16/main.cpp
@@ -1,9 +1,4 @@
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/graphviz.hpp>
-#include <boost/graph/johnson_all_pairs_shortest.hpp>
-#include <boost/graph/named_function_params.hpp>
-#include <boost/graph/properties.hpp>
-#include <boost/property_map/property_map.hpp>
+
 #include <chrono>
 #include <fstream>
 #include <iostream>
@@ -11,157 +6,7 @@
 #include <regex>
 #include <string>
 
-#define TESTING true
-
-// using Nodes = std::map<int, int>;
-// using Edges = std::map<int, std::vector<int>>;
-// using ValveTags = std::map<std::string, int>;
-
-struct ValveInfo {
-  std::string name{};
-  int flow{};
-  std::vector<std::string> neighbours;
-};
-
-using Valves = std::vector<ValveInfo>;
-using Edge = std::pair<int, int>;
-
-struct VertexData {
-  std::string node_name;
-  int flow{0};
-};
-
-struct EdgeData {
-  std::string edge_name;
-  double dist;
-};
-
-#if TESTING
-#define V 10
-#else
-#define V 58
-#endif  // TESTING
-
-void parse_input(Valves& valves) {
-  std::string file_name = (TESTING) ? "../test.txt" : "../input.txt";
-  std::ifstream input{file_name};
-
-  std::string initial_valve{""};
-
-  std::regex full_line{
-      R"(Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z]{2}(?:, [A-Z]{2})*))"};
-
-  while (input && !input.eof()) {
-    std::string line{};
-    std::getline(input, line);
-    std::smatch match;
-
-    if (std::regex_match(line, match, full_line)) {
-      ValveInfo valve{};
-      valve.name = match[1];
-      valve.flow = stoi(match[2]);
-      std::string neighbours = {match[3]};
-      std::smatch submatch;
-      std::regex alpha_links{R"([A-Z]{2})"};
-      while (std::regex_search(neighbours, match, alpha_links)) {
-        valve.neighbours.push_back(match.str());
-        neighbours = match.suffix();
-      }
-      valves.push_back(valve);
-    }
-  }
-}
-
-void build_edges(std::set<Edge>& edges, const Valves& valves) {
-  std::map<std::string, int> edge_map;
-
-  for (int index{0}; auto valve : valves)
-    edge_map.insert(std::make_pair(valve.name, index++));
-
-  for (auto valve : valves) {
-    int source{(edge_map[valve.name])};
-    for (auto neighbour : valve.neighbours) {
-      int destination{edge_map[neighbour]};
-      if (source < destination)  // prevent duplicate paths
-        edges.insert(Edge(source, destination));
-      else
-        edges.insert(Edge(destination, source));
-    }
-  }
-}
-
-void build_vertices(std::vector<VertexData>& vertices, Valves& valves) {
-  for (auto valve : valves) {
-    VertexData vertex{valve.name, valve.flow};
-    vertices.push_back(vertex);
-  }
-}
-
-using CavernGraph = boost::adjacency_list<
-    boost::vecS, boost::vecS, boost::undirectedS, VertexData,
-    boost::property<boost::edge_weight_t, int,
-                    boost::property<boost::edge_weight2_t, int>>>;
-
-void draw_graph(const CavernGraph& graph, const Valves& valves) {
-  std::ofstream fout("../figs/johnson-eg.dot");
-
-  if (!fout) {
-    // Print an error and exit
-    std::cerr << "Uh oh, Sample.txt could not be opened for writing!\n";
-    return;
-  }
-
-  fout << "digraph A {\n"
-       << "  rankdir=LR\n"
-       << "size=\"5,3\"\n"
-       << "ratio=\"fill\"\n"
-       << "edge[style=\"bold\"; arrowhead=\"none\"]\n"
-       << "node[shape=\"circle\"]\n";
-
-  boost::graph_traits<CavernGraph>::edge_iterator ei, ei_end;
-  for (boost::tie(ei, ei_end) = boost::edges(graph); ei != ei_end; ++ei)
-    fout << valves[boost::source(*ei, graph)].name << " -> "
-         << valves[boost::target(*ei, graph)].name
-         << "[label=" << get(boost::edge_weight, graph)[*ei] << "]\n";
-
-  fout << "}\n";
-}
-
-void print_distance_matrix(const int D[][V]) {
-  std::cout << "       ";
-  for (int k = 0; k < V; ++k) std::cout << std::setw(5) << k;
-  std::cout << std::endl;
-  for (int i = 0; i < V; ++i) {
-    std::cout << std::setw(3) << i << " -> ";
-    for (int j = 0; j < V; ++j) {
-      if (D[i][j] == (std::numeric_limits<int>::max)())
-        std::cout << std::setw(5) << "inf";
-      else
-        std::cout << std::setw(5) << D[i][j];
-    }
-    std::cout << std::endl;
-  }
-}
-
-void print_flowing_distance_matrix(const int D[][V],
-                                   const std::vector<int>& working_valves) {
-  std::cout << "       ";
-  for (int k = 0; k < V; ++k)
-    if (working_valves[k] || k == 0) std::cout << std::setw(5) << k;
-  std::cout << std::endl;
-  for (int i = 0; i < V; ++i) {
-    if (!working_valves[i] && i) continue;
-    std::cout << std::setw(3) << i << " -> ";
-    for (int j = 0; j < V; ++j) {
-      if (!working_valves[j] && j) continue;
-      if (D[i][j] == (std::numeric_limits<int>::max)())
-        std::cout << std::setw(5) << "inf";
-      else
-        std::cout << std::setw(5) << D[i][j];
-    }
-    std::cout << std::endl;
-  }
-}
+#include "day16_utils.h"
 
 int calculate_max_flow(std::vector<int> flows) {}
 
@@ -211,12 +56,9 @@ int main(int, char**) {
   std::vector<std::vector<int>> dist_between_working_valves;
 
   for (int i = 0; i < V; ++i) {
-    if (!flow_vertices[i] && i) continue;
     std::vector<int> distance;
-    for (int j = 0; j < V; ++j) {
-      if (!flow_vertices[j] && j) continue;
+    for (int j = 0; j < V; ++j) 
       distance.push_back(D[i][j]);
-    }
     dist_between_working_valves.push_back(distance);
   }
 
