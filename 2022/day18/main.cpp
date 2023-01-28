8a2531762c89d88be1ef2df7dd813f1a4af68696
Douglas Webster <douglas.webster2107@gmail.com>
2023-01-28 18:37:19 +0000

    completed day 18

--- 2022/day18/main.cpp
+++ 2022/day18/main.cpp
@@ -1,10 +1,13 @@
-
+#include <algorithm>
+#include <array>
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <queue>
 #include <regex>
 #include <set>
 #include <string>
+#include <tuple>
 #include <vector>
 
 #include "day18_helpers.h"
@@ -20,17 +23,124 @@
 using BlockAxis = std::vector<int>;
 using Blocks = std::vector<BlockAxis>;
 using Point = std::pair<int, int>;
+using Location3D = std::tuple<int, int, int>;
+using Matrix3D = std::vector<std::vector<std::vector<int>>>;
+using NextVisits = std::vector<Location3D>;
 
-int get_blocks_area (Blocks & blocks) {
+bool is_valid(const Matrix3D& matrix, Location3D index)
+{
+    int x = std::get<0>(index);
+    int y = std::get<1>(index);
+    int z = std::get<2>(index);
 
-    int total_face_count{static_cast<int>(blocks[0].size()) * 6};
+    int x_limit { static_cast<int>(matrix.size()) };
+    int y_limit { static_cast<int>(matrix[0].size()) };
+    int z_limit { static_cast<int>(matrix[0][0].size()) };
+
+    if (x < 0 || y < 0 || z < 0 || x >= x_limit || y >= y_limit || z >= z_limit)
+        return false;
+
+    return (matrix[x][y][z] == 0);
+}
+
+void get_neighbours(const Matrix3D matrix, Location3D current_pos, NextVisits& next_positions)
+{
+    Location3D limits({ matrix.size(), matrix[0].size(), matrix[0][0].size() });
+
+    Location3D next_x_pos { current_pos };
+    std::get<0>(next_x_pos) += 1;
+    Location3D next_x_neg { current_pos };
+    std::get<0>(next_x_neg) -= 1;
+    Location3D next_y_pos { current_pos };
+    std::get<1>(next_y_pos) += 1;
+    Location3D next_y_neg { current_pos };
+    std::get<1>(next_y_neg) -= 1;
+    Location3D next_z_pos { current_pos };
+    std::get<2>(next_z_pos) += 1;
+    Location3D next_z_neg { current_pos };
+    std::get<2>(next_z_neg) -= 1;
+
+    if (is_valid(matrix, next_x_pos))
+        next_positions.push_back(next_x_pos);
+    if (is_valid(matrix, next_x_neg))
+        next_positions.push_back(next_x_neg);
+    if (is_valid(matrix, next_y_pos))
+        next_positions.push_back(next_y_pos);
+    if (is_valid(matrix, next_y_neg))
+        next_positions.push_back(next_y_neg);
+    if (is_valid(matrix, next_z_pos))
+        next_positions.push_back(next_z_pos);
+    if (is_valid(matrix, next_z_neg))
+        next_positions.push_back(next_z_neg);
+}
+
+void flood_fill(Matrix3D& blocks, Location3D starting_point, int replacement_value = 1)
+{
+    int cols { std::get<0>(starting_point) };
+    int rows { std::get<1>(starting_point) };
+    int levels { std::get<2>(starting_point) };
+
+    std::set<Location3D> to_visit;
+    to_visit.insert({ cols, rows, levels });
+    std::set<Location3D> visited;
+    NextVisits next_visits;
+
+    while (to_visit.size() > 0) {
+        next_visits.clear();
+        std::set<Location3D>::iterator it = to_visit.begin();
+        Location3D next = *it;
+        to_visit.erase(it);
+        visited.insert(next);
+        blocks[std::get<0>(next)][std::get<1>(next)][std::get<2>(next)] = replacement_value;
+        get_neighbours(blocks, next, next_visits);
+        for (auto neighbour : next_visits) {
+            bool not_visited { visited.find(neighbour) == visited.end() };
+            if (not_visited)
+                to_visit.insert(neighbour);
+        }
+    }
+}
+
+/// @brief Given a block create an enveloping cube with a border of at
+///  least 1 on every side.
+///  1. Flood fill the outside of this enveloping
+///     cube.
+///  2. find any empty space left and flood fill this, keeping track
+///     of any location filled.
+///  3. Treat this newly flooded area as a new lava
+///     block and get its outside are.
+///  4. repeat 2 & 3 until there are no more void spaces.
+/// @param blocks
+Matrix3D convert_to_filled_map(Blocks& blocks)
+{
+    int x_size = *std::max_element(blocks[0].begin(), blocks[0].end()) + 2;
+    int y_size = *std::max_element(blocks[1].begin(), blocks[1].end()) + 2;
+    int z_size = *std::max_element(blocks[2].begin(), blocks[2].end()) + 2;
+
+    Matrix3D encircling_cube(x_size, std::vector<std::vector<int>>(y_size, std::vector<int>(z_size, 0)));
+
+    for (int index { 0 }; index < blocks[0].size(); ++index) {
+        int x { blocks[0][index] };
+        int y { blocks[1][index] };
+        int z { blocks[2][index] };
+
+        encircling_cube[x][y][z] = 1;
+    }
+
+    return encircling_cube;
+}
+
+int get_blocks_area(Blocks& blocks)
+{
+
+    int total_face_count { static_cast<int>(blocks[0].size()) * 6 };
     int common_face_count { 0 };
 
     // check x & y axis for co-planer blocks
     for (int x_index { 0 }; x_index < static_cast<int>(blocks[0].size()); ++x_index) {
         Point base_point { blocks[0][x_index], blocks[1][x_index] };
         int base_z_ordinate { blocks[2][x_index] };
-        for (int y_index {0}; y_index < static_cast<int>(blocks[1].size()); ++y_index) {
+        for (int y_index { 0 }; y_index < static_cast<int>(blocks[1].size()); ++y_index) {
             Point check_point { blocks[0][y_index], blocks[1][y_index] };
             if (base_point == check_point) {
                 int check_z_ordinate { blocks[2][y_index] };
@@ -74,6 +184,41 @@ int get_blocks_area (Blocks & blocks) {
     return total_face_count - common_face_count;
 }
 
+Location3D get_void_point(const Matrix3D& blocks)
+{
+    Location3D no_point { -1, -1, -1 };
+
+    for (int x { 0 }; auto row : blocks) {
+        for (int y { 0 }; auto col : row) {
+            for (int z { 0 }; auto level : col) {
+                if (level == 0)
+                    return Location3D { x, y, z };
+                ++z;
+            }
+            ++y;
+        }
+        ++x;
+    }
+
+    return no_point;
+}
+
+void get_void_points(const Matrix3D matrix, Blocks& void_blocks)
+{
+    int x_limit { static_cast<int>(matrix.size()) - 1 };
+    int y_limit { static_cast<int>(matrix[0].size()) - 1 };
+    int z_limit { static_cast<int>(matrix[0][0].size()) - 1 };
+
+    for (int x { 1 }; x < x_limit; ++x)
+        for (int y { 1 }; y < y_limit; ++y)
+            for (int z { 1 }; z < z_limit; ++z)
+                if (matrix[x][y][z] == 0) {
+                    void_blocks[0].push_back(x);
+                    void_blocks[1].push_back(y);
+                    void_blocks[2].push_back(z);
+                }
+}
+
 /// @brief
 /// @param
 /// @param
@@ -82,9 +227,6 @@ int main(int, char**)
 {
     auto time_start = std::chrono::high_resolution_clock::now();
     std::ifstream data { FILE };
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
-        end_time - time_start);
 
     if (!data) {
         std::cerr << FILE << " could not be opened for reading\n ";
@@ -108,13 +250,28 @@ int main(int, char**)
     }
 
     // Part 1
-    int part1_area{get_blocks_area(blocks)};
+    int part1_area { get_blocks_area(blocks) };
 
-    
     std::cout << "Total number of faces by counting: " << part1_area << '\n';
 
+    Matrix3D encircling_cube { convert_to_filled_map(blocks) };
+
+    flood_fill(encircling_cube, Location3D { 0, 0, 0 });
+
+    Blocks void_areas(3);
+
+    get_void_points(encircling_cube, void_areas);
+
+    int void_area {get_blocks_area(void_areas)};
+
+    int part2_area = part1_area - void_area;
+
+    std:: cout << "Total external area: " << part2_area << '\n';
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        end_time - time_start);
     std::cout << "Time taken by program: " << duration.count() << " microseconds"
               << "\n";
     return 0;
 }
-
